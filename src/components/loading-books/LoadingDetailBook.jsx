aaebc58e0eecaa2f8161bd1531920b7713eea350
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-04-04 00:52:00 +0700

    add amimation loading detail book

---
+++ src/components/loading-books/LoadingDetailBook.jsx
@@ -0,0 +1,36 @@
+function LoadingDetailBook() {
+  return (
+    <>
+      <div className="animate-pulse flex">
+        <div className="basis-2/3 w-[70vw]">
+          <div className="bg-slate-200 rounded h-4 mb-1"></div>
+          <div className="bg-slate-200 rounded h-3 w-[30rem] mb-1"></div>
+          <div className="flex mb-3">
+            <div className="bg-slate-200 rounded h-1 w-16 mr-2"></div>
+            <div className="bg-slate-200 rounded h-1 w-16"></div>
+          </div>
+          <div className="bg-slate-200 rounded h-3 mb-1 w-20"></div>
+          <div className="bg-slate-200 rounded h-2 mb-1"></div>
+          <div className="bg-slate-200 rounded h-2 mb-1"></div>
+          <div className="bg-slate-200 rounded h-2 mb-1"></div>
+          <div className="bg-slate-200 rounded h-2 mb-1"></div>
+          <div className="bg-slate-200 rounded h-2 mb-1"></div>
+          <div className="bg-slate-200 rounded h-2 mb-1"></div>
+          <div className="h-12 flex items-center">
+            <div className="bg-slate-200 rounded h-2 w-6 mr-2"></div>
+            <div className="bg-slate-200 rounded h-2 w-6 mr-2"></div>
+            <div className="bg-slate-200 rounded h-2 w-6"></div>
+          </div>
+          <div className="flex">
+            <div className="bg-slate-200 rounded h-5 w-24 mr-2"></div>
+            <div className="bg-slate-200 rounded h-5 w-24"></div>
+          </div>
+        </div>
+        <div className="basis-1/3 grid justify-items-center">
+          <div className="h-[20rem] bg-slate-200 rounded-lg w-[14rem]"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default LoadingDetailBook;
--- src/pages/DetailBook.jsx
+++ src/pages/DetailBook.jsx
@@ -5,6 +5,7 @@ import { getBookDetail } from "../redux/slice/books-slice";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import ButtonSaveBook from "../components/book-save/ButtonSaveBook";
+import LoadingDetailBook from "../components/loading-books/LoadingDetailBook";
 
 function DetailBook() {
   const { id } = useParams();
@@ -47,121 +48,133 @@ function DetailBook() {
       <Navbar />
       <div className="section">
         <div className="px-16 lg:px-6 pt-20 pb-8 flex">
-          {bookDetail && bookDetail.volumeInfo ? (
+          {status === "loading" ? (
             <>
-              <div className="basis-2/3 text-[#525E85]">
-                <div className="text-3xl font-bold mb-0.5">
-                  {bookDetail.volumeInfo.title}
-                </div>
-                <div className="font-medium text-lg text-[#8fabff] mb-0.5">
-                  {bookDetail.volumeInfo.authors ? (
-                    <>
-                      {bookDetail.volumeInfo.authors.length > 1
-                        ? "Authors: "
-                        : "Author: "}
-                      {bookDetail.volumeInfo.authors &&
-                        bookDetail.volumeInfo.authors.join(", ")}
-                    </>
-                  ) : (
-                    <></>
-                  )}
-                </div>
-                <div className="text-sm mb-3">
-                  {bookDetail.volumeInfo.publishedDate
-                    ? `${formatDate(bookDetail.volumeInfo.publishedDate)}`
-                    : "eBook"}{" "}
-                  - {bookDetail.volumeInfo.publisher}
-                </div>
-                <div className="text-base font-medium text-justify">
-                  <div className="font-bold text-lg">About this book:</div>
-                  {bookDetail.volumeInfo.description ? (
-                    <div
-                      dangerouslySetInnerHTML={{
-                        __html: bookDetail.volumeInfo.description,
-                      }}
-                    />
-                  ) : (
-                    <div>{bookDetail.volumeInfo.subtitle}</div>
-                  )}
-                </div>
-                <div className="h-18 flex items-center font-bold text-lg">
-                  <div className="px-2">
-                    {bookDetail.saleInfo.saleability === "FREE"
-                      ? "Free book"
-                      : bookDetail.saleInfo.saleability === "FOR_SALE"
-                      ? `${formattedAmount(
-                          bookDetail.saleInfo.listPrice.amount
-                        )} `
-                      : "Not for sale"}
-                  </div>
-                  |
-                  <div className="px-2">
-                    {bookDetail.volumeInfo.language === "id"
-                      ? " Indonesia"
-                      : " English"}
-                  </div>
-                  |
-                  <div className="px-2">
-                    {bookDetail.volumeInfo.pageCount} pages
-                  </div>
-                </div>
-                <div className="flex">
-                  <div className="cursor-pointer button-read rounded-lg bg-[#525E85] text-white font-bold mr-2 hover:bg-white hover:text-[#525E85] border border-2 hover:border-[#525E85]">
-                    {bookDetail.saleInfo.saleability === "FREE" ? (
-                      <div
-                        className="py-1 flex justify-center w-24"
-                        onClick={() =>
-                          buttonLink(bookDetail.accessInfo.pdf.downloadLink)
+              <LoadingDetailBook />
+            </>
+          ) : (
+            <>
+              {bookDetail && bookDetail.volumeInfo ? (
+                <>
+                  <div className="basis-2/3 text-[#525E85]">
+                    <div className="text-3xl font-bold mb-0.5">
+                      {bookDetail.volumeInfo.title}
+                    </div>
+                    <div className="font-medium text-lg text-[#8fabff] mb-0.5">
+                      {bookDetail.volumeInfo.authors ? (
+                        <>
+                          {bookDetail.volumeInfo.authors.length > 1
+                            ? "Authors: "
+                            : "Author: "}
+                          {bookDetail.volumeInfo.authors &&
+                            bookDetail.volumeInfo.authors.join(", ")}
+                        </>
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+                    <div className="text-sm mb-3">
+                      {bookDetail.volumeInfo.publishedDate
+                        ? `${formatDate(bookDetail.volumeInfo.publishedDate)}`
+                        : "eBook"}{" "}
+                      - {bookDetail.volumeInfo.publisher}
+                    </div>
+                    <div className="text-base font-medium text-justify">
+                      <div className="font-bold text-lg">About this book:</div>
+                      {bookDetail.volumeInfo.description ? (
+                        <div
+                          dangerouslySetInnerHTML={{
+                            __html: bookDetail.volumeInfo.description,
+                          }}
+                        />
+                      ) : (
+                        <div>{bookDetail.volumeInfo.subtitle}</div>
+                      )}
+                    </div>
+                    <div className="h-18 flex items-center font-bold text-lg">
+                      <div className="px-2">
+                        {bookDetail.saleInfo.saleability === "FREE"
+                          ? "Free book"
+                          : bookDetail.saleInfo.saleability === "FOR_SALE"
+                          ? `${formattedAmount(
+                              bookDetail.saleInfo.listPrice.amount
+                            )} `
+                          : "Not for sale"}
+                      </div>
+                      |
+                      <div className="px-2">
+                        {bookDetail.volumeInfo.language === "id"
+                          ? " Indonesia"
+                          : " English"}
+                      </div>
+                      |
+                      <div className="px-2">
+                        {bookDetail.volumeInfo.pageCount} pages
+                      </div>
+                    </div>
+                    <div className="flex">
+                      <div className="cursor-pointer button-read rounded-lg bg-[#525E85] text-white font-bold mr-2 hover:bg-white hover:text-[#525E85] border border-2 hover:border-[#525E85]">
+                        {bookDetail.saleInfo.saleability === "FREE" ? (
+                          <div
+                            className="py-1 flex justify-center w-24"
+                            onClick={() =>
+                              buttonLink(bookDetail.accessInfo.pdf.downloadLink)
+                            }
+                          >
+                            Download
+                          </div>
+                        ) : bookDetail.saleInfo.saleability === "FOR_SALE" ? (
+                          <div
+                            className="py-1 flex justify-center w-24 "
+                            onClick={() =>
+                              buttonLink(bookDetail.saleInfo.buyLink)
+                            }
+                          >
+                            Buy
+                          </div>
+                        ) : (
+                          <div
+                            className="py-1 flex justify-center w-24"
+                            onClick={() =>
+                              buttonLink(bookDetail.volumeInfo.infoLink)
+                            }
+                          >
+                            Preview
+                          </div>
+                        )}
+                      </div>
+                      <ButtonSaveBook
+                        bookDetail={bookDetail}
+                        isSaved={isSaved}
+                        setIsSaved={setIsSaved}
+                        updateSavedBooks={updateSavedBooks}
+                        styleIcon={"h-3.5 self-center pr-0.5"}
+                        styleButton={
+                          "cursor-pointer button-read border border-2 rounded-lg border-[#525E85] font-bold w-24 flex justify-center py-1"
                         }
-                      >
-                        Download
-                      </div>
-                    ) : bookDetail.saleInfo.saleability === "FOR_SALE" ? (
-                      <div
-                        className="py-1 flex justify-center w-24 "
-                        onClick={() => buttonLink(bookDetail.saleInfo.buyLink)}
-                      >
-                        Buy
-                      </div>
+                      />
+                    </div>
+                  </div>
+                  <div className="basis-1/3 grid justify-items-center">
+                    {bookDetail.volumeInfo.imageLinks ? (
+                      <img
+                        src={bookDetail.volumeInfo.imageLinks?.thumbnail}
+                        alt={bookDetail.title}
+                        className="img-swipe h-[20rem]"
+                      />
                     ) : (
-                      <div
-                        className="py-1 flex justify-center w-24"
-                        onClick={() =>
-                          buttonLink(bookDetail.volumeInfo.infoLink)
-                        }
-                      >
-                        Preview
+                      <div className="text-center  bg-gray-300 py-5 mx-6">
+                        {bookDetail.title}
                       </div>
                     )}
                   </div>
-                  <ButtonSaveBook
-                    bookDetail={bookDetail}
-                    isSaved={isSaved}
-                    setIsSaved={setIsSaved}
-                    updateSavedBooks={updateSavedBooks}
-                    styleIcon={"h-3.5 self-center pr-0.5"}
-                    styleButton={
-                      "cursor-pointer button-read border border-2 rounded-lg border-[#525E85] font-bold w-24 flex justify-center py-1"
-                    }
-                  />
+                </>
+              ) : (
+                <div className="text-[#525E85] font-bold">
+                  Cannot found this book
                 </div>
-              </div>
-              <div className="basis-1/3 grid justify-items-center">
-                {bookDetail.volumeInfo.imageLinks ? (
-                  <img
-                    src={bookDetail.volumeInfo.imageLinks?.thumbnail}
-                    alt={bookDetail.title}
-                    className="img-swipe h-[20rem]"
-                  />
-                ) : (
-                  <div className="text-center  bg-gray-300 py-5 mx-6">
-                    {bookDetail.title}
-                  </div>
-                )}
-              </div>
+              )}
             </>
-          ) : (
-            <p>Loading...</p>
           )}
         </div>
       </div>
